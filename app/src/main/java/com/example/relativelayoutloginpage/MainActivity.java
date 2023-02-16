2e9b9bb8845f7765954f88349f68d86aa031f01b
KiranKumarMalik <malikkiran413@gmail.com>
2023-02-17 00:14:33 +0530

    First commit

---
+++ app/src/main/java/com/example/relativelayoutloginpage/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.relativelayoutloginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
