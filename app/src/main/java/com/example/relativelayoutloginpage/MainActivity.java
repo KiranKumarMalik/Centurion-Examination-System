b232dd6d575e88c497281a7e0fd35cbe0421a9cf
KiranKumarMalik <malikkiran413@gmail.com>
2023-02-20 00:06:33 +0530

    adding student home page

--- app/src/main/java/com/example/relativelayoutloginpage/MainActivity.java
+++ app/src/main/java/com/example/relativelayoutloginpage/MainActivity.java
@@ -4,17 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.student_homepage);
 
 
 
