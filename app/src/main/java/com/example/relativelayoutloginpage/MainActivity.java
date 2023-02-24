e261790480f0f0d9e0484948b2761f30cb80e91f
KiranKumarMalik <malikkiran413@gmail.com>
2023-02-24 10:23:29 +0530

    adding new activity

--- app/src/main/java/com/example/relativelayoutloginpage/MainActivity.java
+++ app/src/main/java/com/example/relativelayoutloginpage/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.relativelayoutloginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView exam, notice, result, admitcard, profile;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_homepage);
+
+        exam = (CardView) findViewById(R.id.exam_card);
+        notice = (CardView) findViewById(R.id.notice_card);
+        result = (CardView) findViewById(R.id.result_card);
+        admitcard = (CardView) findViewById(R.id.admit_card);
+        profile = (CardView) findViewById(R.id.profile_card);
 
 
 
