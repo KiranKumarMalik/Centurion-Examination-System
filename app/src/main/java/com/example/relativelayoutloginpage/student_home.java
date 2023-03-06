761fd85f701099b0c0355ea0c98da83a069519db
KiranKumarMalik <malikkiran413@gmail.com>
2023-03-06 20:51:47 +0530

    adding login function

---
+++ app/src/main/java/com/example/relativelayoutloginpage/student_home.java
@@ -0,0 +1,14 @@
+package com.example.relativelayoutloginpage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class student_home extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_homepage);
+    }
+}
