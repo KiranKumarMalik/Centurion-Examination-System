89073472b87d1f5e128f31465fc162a9bbf70c7d
KiranKumarMalik <malikkiran413@gmail.com>
2023-04-07 10:33:34 +0530

    Adding student notice activity

---
+++ app/src/main/java/com/example/relativelayoutloginpage/Student_notice.java
@@ -0,0 +1,14 @@
+package com.example.relativelayoutloginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Student_notice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_notice);
+    }
+}
