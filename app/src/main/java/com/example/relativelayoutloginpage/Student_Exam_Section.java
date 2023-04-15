53be4371b9905908758e0868de1096eb408b4043
KiranKumarMalik <malikkiran413@gmail.com>
2023-04-15 20:41:42 +0530

    new changes in exam section in student

---
+++ app/src/main/java/com/example/relativelayoutloginpage/Student_Exam_Section.java
@@ -0,0 +1,14 @@
+package com.example.relativelayoutloginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Student_Exam_Section extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_exam_section);
+    }
+}
